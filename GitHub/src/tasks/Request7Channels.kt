ccaba3fa1e5feaa7b0f493e9eab3c3f1d1d0bf1e
Jungsun Kim <kimjs@hanyang.ac.kr>
2022-09-25 18:29:58 +0900

    Kotlin 1.7.10

--- GitHub/src/tasks/Request7Channels.kt
+++ GitHub/src/tasks/Request7Channels.kt
@@ -1,21 +1,15 @@
 package tasks
 
-import contributors.GitHubService
-import contributors.RequestData
-import contributors.User
-import kotlinx.coroutines.coroutineScope
-
 /**
  * Implement the function `loadContributorsChannels` that requests all the GitHub contributors
  * concurrently, but shows intermediate progress at the same time. Use these two previous
  * functions: `loadContributorsConcurrent` from Request5Concurrent.kt and `loadContributorsProgress`
  * from Request6Progress.kt.
  */
-suspend fun loadContributorsChannels(service: GitHubService,
-                                     req: RequestData,
-                                     updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) = coroutineScope {
-
-    TODO() as Unit // temporary
-}
-
+//suspend fun loadContributorsChannels(service: GitHubService,
+//                                     req: RequestData,
+//                                     updateResults: suspend (List<User>, completed: Boolean) -> Unit
+//) = coroutineScope {
+//
+//    TODO() as Unit // temporary
+//}
